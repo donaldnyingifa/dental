bf3bf334d0df00e26d559c050bb13c7edf689544
Donald Nyingifa <donaldnyingifa@gmail.com>
2020-04-01 22:07:29 +0100

    tweaked app for heroku

--- dental/settings.py
+++ dental/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+#PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
